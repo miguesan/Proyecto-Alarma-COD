2cf9d29ddbd30b9f0c3671a21e17e5374e66c306
Miguel Sanchez Blanco <msanchezblanco@danielcastelao.org>
2017-05-04 09:14:37 +0200

    Definidos los metodos showHR, showAlarm y showLeds

---
+++ MiniProyecto/src/miniproyecto/Display.java
@@ -0,0 +1,22 @@
+
+package miniproyecto;
+
+/**
+ *
+ * @author Miguel Sánchez Blanco
+ */
+public class Display {
+    
+    public void showHR(){
+    
+    }
+    
+    public void showAlarm(){
+        
+    }
+    
+    public void showLeds(){
+        
+    }
+    
+}
