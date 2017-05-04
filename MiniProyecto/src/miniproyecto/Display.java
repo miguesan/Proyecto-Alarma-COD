60db01651585f47feab6ea0de41043db0d8207e8
Miguel Sanchez Blanco <msanchezblanco@danielcastelao.org>
2017-05-04 10:06:27 +0200

    Creando JavaDOC

--- MiniProyecto/src/miniproyecto/Display.java
+++ MiniProyecto/src/miniproyecto/Display.java
@@ -1,24 +1,39 @@
-
+/**
+ * This class 'Display' contains the methods and parameters that will be displayed on the application screen
+ */
 package miniproyecto;
 
-/**
- *
+/** 
+ * In the constructor you define the variables 
  * @author Miguel Sánchez Blanco
  */
+
 public class Display {
     
     boolean ledClock;
     boolean ledAlarm;
     boolean ledSET;
     
+    /**
+     * The method 'showClock' shows the current time
+     */
     public void showClock(){
         
     }
     
+    /**
+     * The method 'showAlarm' shows the alarm time
+     */
     public void showAlarm(){
         
     }
     
+    /**
+     * The method 'showLeds' shows the LEDs of the active functions
+     * @param showClock
+     * @param showAlarm
+     * @param showSET 
+     */
     public void showLeds(boolean showClock,boolean showAlarm, boolean showSET){
         
         ledClock = showClock;
