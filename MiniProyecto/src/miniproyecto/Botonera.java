5866366fd29b16af1941e68dd17d6f5f6d4943ac
Miguel Sanchez Blanco <msanchezblanco@danielcastelao.org>
2017-05-04 10:34:02 +0200

    Creando JavaDOC

--- MiniProyecto/src/miniproyecto/Botonera.java
+++ MiniProyecto/src/miniproyecto/Botonera.java
@@ -1,8 +1,10 @@
-
+/**
+ * The 'Botonera' class contains the methods for all "buttons" to contain the program
+ */
 package miniproyecto;
 
 /**
- *
+ * The constructor defines the 'alarmActive', 'setHR' and 'setAlarm' variables as type boolean
  * @author Miguel Sánchez Blanco
  */
 public class Botonera {
@@ -11,30 +13,51 @@ public class Botonera {
     boolean setHR;
     boolean setAlarm;
     
+    /**
+     * The 'alarmaON' method turns on the alarm
+     */
     public void alarmaON(){
         
     }
     
+    /**
+     * The 'alarmaOFF' method turns off the alarm
+     */
     public void alarmaOFF(){
         
     }
     
+    /**
+     * The 'confHR' method sets the time
+     */
     public void confHR(){
         
     }
     
+    /**
+     * The 'confAlarma' method sets the alarm time
+     */
     public void confAlarma(){
         
     }
     
+    /**
+     * 'plusHR' method increments time
+     */
     public void plusHR(){
         
     }
     
+    /**
+     * 'plusMin' method increases minutes
+     */
     public void plusMin(){
         
     }
     
+    /**
+     * The 'stopAlarm' method stops the alarm
+     */
     public void stopAlarm(){
         
     }
