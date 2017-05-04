94d26ab712107d7103ac6f37383db629eeefc63d
Miguel Sanchez Blanco <msanchezblanco@danielcastelao.org>
2017-05-04 09:13:55 +0200

    Definidos variables alarmaActive, setHR y setAlarm como boolean y los metodos  alarmaON, alarmaOFF, confHR, confAlarma, plusHR, plusMin y stopAlarm

---
+++ MiniProyecto/src/miniproyecto/Botonera.java
@@ -0,0 +1,44 @@
+
+package miniproyecto;
+
+/**
+ *
+ * @author Miguel Sánchez Blanco
+ */
+public class Botonera {
+    
+    boolean alarmActive;
+    boolean setHR;
+    boolean setAlarm;
+    
+    public void alarmaON(){
+        
+    }
+    
+    public void alarmaOFF(){
+        
+    }
+    
+    public void confHR(){
+        
+    }
+    
+    public void confAlarma(){
+        
+    }
+    
+    public void plusHR(){
+        
+    }
+    
+    public void plusMin(){
+        
+    }
+    
+    public void stopAlarm(){
+        
+    }
+    
+    
+    
+}
